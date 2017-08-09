6b624d8949e9da7606e69dd24810fae3347f672b
Kevin Broløs <kevin.broloes@bestbrains.dk>
2017-08-09 17:16:06 +0200

    Add tests for ec

--- helpers/ec.rb
+++ helpers/ec.rb
@@ -14,35 +14,28 @@ class EllipticCurve
 
     @pkey = ec.private_key.to_s 16 # Returns BigNum, convert to base16 (hex)
     @pub = ec.public_key.to_bn.to_s 16 # Convert to BigNum so we can get the hex value
-
-    puts "Keypair:"
-    puts "Priv: #{pkey}"
-    puts "Publ: #{pub}"
   end
 
-  def pretty_print(pub)
+  def get_components(pub)
     block_size = 32 * 2  # 32 bytes in hex
     type_size = 2        # 1 byte in hex
 
     key = pub.split(//) # Convert to char array
-
-    type = key.take(type_size).join
-    x = key.drop(type_size).take(block_size).join
-    y = key.drop(type_size + block_size).take(block_size).join if type.eql? UNCOMPRESSED
-
-    puts "\nType: #{type}"
-    puts "x: #{x}, size: #{x.size }"
-    puts "y: #{y}, size: #{y.size }" if type.eql? UNCOMPRESSED
+    {
+        :type => key.take(type_size).join,
+        :x => key.drop(type_size).take(block_size).join,
+        :y => key.drop(type_size + block_size).take(block_size).join
+    }
   end
 
-  def compressed(pub)
+  def get_compressed(pub)
     key = pub.split(//) # Convert to char array
     x = key.drop(2).take(64).join
 
     # No reason to send the whole number for testing
     y_ending = key.drop(2 + 64 + 63).join
 
-    get_prefix(y_ending).concat x
+    get_prefix(y_ending) + x
   end
 
   def get_prefix(key)
--- main.rb
+++ main.rb
@@ -10,9 +10,23 @@ puts "Sha256:" + digest
 def main
   ec = EllipticCurve.new
 
+  puts "Keypair:"
+  puts "Priv: #{ec.pkey}"
+  puts "Publ: #{ec.pub}"
+
   pub = ec.pub
-  ec.pretty_print(pub)
-  ec.pretty_print(ec.compressed(pub))
+  components = ec.get_components(pub)
+  pretty_print components
+
+  components = ec.get_components(ec.get_compressed(pub))
+  pretty_print components
+
+end
+
+def pretty_print (pub_components)
+  puts "\nType: #{pub_components[:type]}"
+  puts "x: #{pub_components[:x]}, size: #{pub_components[:x].size }"
+  puts "y: #{pub_components[:y]}, size: #{pub_components[:y].size }" if pub_components[:y].size != 0
 end
 
 main()
